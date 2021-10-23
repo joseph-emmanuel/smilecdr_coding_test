ee05c4b2b5e34a13296bb35578378fa89a97de94
Joseph Emmanuel <joseph.emmanuel@outlook.com>
2021-10-23 16:55:00 -0400

    completed till adding a home page and installing dom for routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-    React app
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
