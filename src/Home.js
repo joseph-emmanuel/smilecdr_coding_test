23466e926410dda2663b33bab886720fa516c90f
Joseph Emmanuel <joseph.emmanuel@outlook.com>
2021-10-25 15:07:54 -0400

    complete code before submitting

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import './Home.css';
 
 function Home() {
     return (
-        <div>Home            
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">Cover</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="/patient">Patients</a>
+            <a class="nav-link" href="#">Create Patients</a>
+          </nav>
         </div>
-    )
+      </header>
+    </div>
+
+
+   
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    
+  </body>
+
+    )``
 }
 
 export default Home
